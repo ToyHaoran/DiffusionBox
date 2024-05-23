02c41f19cffd86256d756da4a49eb8444151f476
李浩然 <lihaoran2048@qq.com>
2024-05-23 21:41:57 +0800

    update

--- demo/demo.py
+++ demo/demo.py
@@ -30,7 +30,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "--output-folder",
-    default="demo/visualization/base",
+    default="demo/visualization/",
     help="where to store the visulization result.",
 )
 parser.add_argument(
@@ -44,42 +44,50 @@ parser.add_argument(
     help="if True, output a video.",
 )
 parser.add_argument(
-    "--track_refs",
+    "--track-refs",
     action="store_true",
     help="if True, output ref boxes for each current boxes.",
+)
+parser.add_argument(
+    "--start-frame",
+    type=int,
+    default=0,
+    help="demo inference starts from this frame_id",
 )
 
 args = parser.parse_args()
 cfg.merge_from_file("configs/BASE_RCNN_1gpu.yaml")
-if 'Diffusion' in args.config:
-    from mega_core.modeling.detector.diffusion_det import add_diffusiondet_config
-    add_diffusiondet_config(cfg)
-    cfg.merge_from_file(args.config)
-    cfg.MODEL.VID.MEGA.MIN_OFFSET = -0
-    cfg.MODEL.VID.MEGA.MAX_OFFSET = 0
-    cfg.MODEL.VID.MEGA.ALL_FRAME_INTERVAL = 1
-    cfg.MODEL.VID.MEGA.KEY_FRAME_LOCATION = 0
-    cfg.INPUT.INFER_BATCH = 1
-else:
-    cfg.merge_from_file(args.config)
+# if 'Diffusion' in args.config:
+from mega_core.modeling.detector.diffusion_det import add_diffusiondet_config
+add_diffusiondet_config(cfg)
+cfg.merge_from_file(args.config)
+cfg.MODEL.VID.MEGA.MIN_OFFSET = -0
+cfg.MODEL.VID.MEGA.MAX_OFFSET = 0  # 原0
+cfg.MODEL.VID.MEGA.ALL_FRAME_INTERVAL = 1  # 原1
+cfg.MODEL.VID.MEGA.KEY_FRAME_LOCATION = 0
+cfg.INPUT.INFER_BATCH = 1  # 原1
+# else:
+#     cfg.merge_from_file(args.config)
 cfg.merge_from_list(["MODEL.WEIGHT", args.checkpoint])  # load checkpoint path
 
 name = os.path.split(args.config)[-1].split('.')[0]
 
-model_name = cfg.MODEL.WEIGHT.split('/')[-2]
-args.output_folder = 'visualizations2/' + cfg.MODEL.VID.METHOD + '_' + model_name + '_' + args.visualize_path[-12:]
+model_name = os.path.join(cfg.MODEL.WEIGHT.split('/')[-2] + '_' +  cfg.MODEL.WEIGHT.split('/')[-1][:-4])
+args.output_folder = args.output_folder + '/' + cfg.MODEL.VID.METHOD + '/' + model_name + '_' + args.visualize_path[-12:]
 
 vid_demo = VIDDemo(
     cfg,
+    video=args.video,
     method=cfg.MODEL.VID.METHOD,
-    confidence_threshold=0.2,
+    confidence_threshold=0.3,
     output_folder=args.output_folder
 )
 
 if not args.video:
-    visualization_results = vid_demo.run_on_image_folder(args.visualize_path, suffix=args.suffix, track_refs=args.track_refs)
+    visualization_results = vid_demo.run_on_image_folder(
+        args.visualize_path, suffix=args.suffix, track_refs=args.track_refs, start_frame=args.start_frame)
 else:
-    visualization_results = vid_demo.run_on_video(args.visualize_path)
+    visualization_results = vid_demo.run_on_video(args.visualize_path, start_frame=args.start_frame)
 
 if not args.output_video:
     vid_demo.generate_images(visualization_results)
